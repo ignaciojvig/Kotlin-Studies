6de7adf03113f99fa8a53c509d9fdd3a3947fc2a
João Victor Ignacio <joao.ignacio@venturus.org.br>
2018-10-02 19:33:07 -0300

    Int Double and Float Tests

---
+++ src/main/kotlin/float_double_int.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+    val number: Int = 2
+    val decimalNumber: Double = 10.6
+    val floatNumber: Float = 10.6f
+
+    val answerDouble: Double = decimalNumber * 3
+    val answerFloat: Float = floatNumber * 3
+
+    println("Double: " + answerDouble)
+    println("Float: " + answerFloat)
+}
